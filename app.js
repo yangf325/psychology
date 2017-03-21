7aef8c5293704bc9477a11be18a18bc86661b2dd
Fun <yangfan.avi@gmail.com>
2017-03-21 14:39:06 +0800

    routers all right

--- app.js
+++ app.js
@@ -8,24 +8,9 @@ var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000;
 var app = express();
 
-//加载本地控制器
-var index=require('./lib/controller/indexCon');
-var testMain=require('./lib/controller/testMainCon');
-var testPage=require('./lib/controller/pageCon');
-var testResult=require('./lib/controller/resultCon');
-var artList=require('./lib/controller/artListCon');
-var artDist=require('./lib/controller/artDistCon');
-var messBoard=require('./lib/controller/messCon');
-
-app.use('/index',index.test);
-app.use('/testMain',testMain.test);
-app.use('/testPage',testPage.test);
-app.use('/testResult',testResult.test);
-app.use('/artList',artList.test);
-app.use('/artDist',artDist.test);
-app.use('/messBoard',messBoard.test);
-
-
+//加载本地路由
+var rooter=require('./rooter');
+app.use('/',rooter);
 
 // mongoose.Promise = require('bluebird');
 // mongoose.connect('mongodb://localhost:27017/psychology');
@@ -33,14 +18,13 @@ app.use('/messBoard',messBoard.test);
 app.set('views', path.join(__dirname, '/public/views/pages'));
 app.set('view engine','jade');
 
-app.use(bodyParser.json());
+app.use(bodyParser.json());//数据表单格式化
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(serverStatic('public'));
+
+// app.use(serverStatic('public'));
+
+app.use(express.static(path.join(__dirname,'public')));
 
 app.listen(port);
 console.log('Server is running at '+port);
 
-app.get('/index',function(req,res){
-	console.log('rended');
-	res.render('index',{title:'心理测评系统'});
-})
---
+++ rooter.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var myRoot = express.Router();
+
+
+// 载入控制器
+var indexCon = require('./lib/controller/indexCon');
+var artDistCon = require('./lib/controller/artDistCon');
+var artListCon = require('./lib/controller/artListCon');
+var testMainCon = require('./lib/controller/testMainCon');
+var testPageCon = require('./lib/controller/testPageCon');
+var testResultCon = require('./lib/controller/testResultCon');
+var messBoardCon = require('./lib/controller/messBoardCon');
+
+myRoot.get('/index',indexCon.test);
+
+
+
+
+
+
+
+
+
+module.exports = myRoot; 
