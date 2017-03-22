2fbebdbfe710ee5deba8f7dccdfd86c523d0f40f
Fun <yangfan.avi@gmail.com>
2017-03-22 15:22:30 +0800

    首页全部OK

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var path = require('path');
 var _ = require('underscore');
 var mongoose = require('mongoose');
-// var Movie = require('./models/movie')
 var serverStatic = require('serve-static');
 var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000;
@@ -13,15 +12,13 @@ var rooter=require('./rooter');
 app.use('/',rooter);
 
 // mongoose.Promise = require('bluebird');
-// mongoose.connect('mongodb://localhost:27017/psychology');
+mongoose.connect('mongodb://localhost:27017/psy');
 
 app.set('views', path.join(__dirname, '/public/views/pages'));
 app.set('view engine','jade');
 
 app.use(bodyParser.json());//数据表单格式化
 app.use(bodyParser.urlencoded({extended:true}));
-
-// app.use(serverStatic('public'));
 
 app.use(express.static(path.join(__dirname,'public')));
 app.use(express.static(path.join(__dirname,'bower_components')));
--- public/js/index.js
+++ public/js/index.js
@@ -1,32 +1,73 @@
-var inf={
-	curp:1,
+var inf = {
+	curp: 1,
 }
-var ele={
-	leftArr:$('.index-left'),
-	rightArr:$('.index-right'),
-	lis:$('.psy-li'),
-	ul:$('.psy-ul'),
+var ele = {
+	leftArr: $('.index-left'),
+	rightArr: $('.index-right'),
+	lis: $('.psy-li'),
+	ul: $('.psy-ul'),
+}
+var funs = {
+	lisMove: function(speed, num) {
+		speed = speed || 500;
+		num = num || 0;
+		for (var i = ele.lis.length - 1; i >= 0; i--) {
+			if (i == inf.curp + num) {
+				ele.lis.eq(i).addClass('biggerLi').delay(600);
+				ele.lis.eq(i).animate({
+					left: 100 * (i - inf.curp) + 50 + '%'
+				}, speed, function() {
+					ele.leftArr.on('click', funs.leftEvent);
+					ele.rightArr.on('click', funs.rightEvent);
+				});
+				setTimeout(function() {
+					ele.lis.removeClass('biggerLi');
+				}, 600 + speed);
+			} else {
+				ele.lis.eq(i).animate({
+					left: 100 * (i - inf.curp) + 50 + '%',
+				}, speed, 'swing');
+			}
+		}
+	},
+	fastMove: function() {
+		for (var i = ele.lis.length - 1; i >= 0; i--) {
+			ele.lis.eq(i).animate({
+				'left': 100 * (i - inf.curp) + 50 + '%'
+			}, 0);
+		}
+	},
+	leftEvent: function() {
+		ele.leftArr.off('click', funs.leftEvent);
+		ele.rightArr.off('click', funs.rightEvent);
+		inf.curp++;
+		funs.lisMove(500, -1);
+		if (inf.curp > 3) {
+			inf.curp = 1;
+			funs.fastMove();
+		}
+	},
+	rightEvent: function() {
+		ele.leftArr.off('click', funs.leftEvent);
+		ele.rightArr.off('click', funs.rightEvent);
+		inf.curp--;
+		funs.lisMove(500, 1);
+		if (inf.curp < 1) {
+			inf.curp = 3;
+			funs.fastMove();
+		}
+	}
 }
 
-$('.index').css({'width':window.screen.availWidth,'overflow':'hidden'})
-ele.ul.prepend($(ele.lis[2]).clone());//创建无缝副本
+$('.index').css({
+	'width': window.screen.availWidth,
+	'overflow': 'hidden'
+})
+ele.ul.prepend($(ele.lis[2]).clone()); //创建无缝副本
 ele.ul.append($(ele.lis[0]).clone());
-ele.lis=$('.psy-li');
-inf.curp=2;
-ele.lis.each(function(i){
-	ele.lis.eq(i).css('left',i*50+'%')
-})
+ele.lis = $('.psy-li');
+inf.curp = 2;
+funs.fastMove();
 
-ele.leftArr.click(function(){
-	console.log(ele.lis.eq(inf.curp).css('left'));
-	ele.lis.eq(inf.curp).animate({
-		left:ele.lis.eq(inf.curp).css('left').slice('%')[0]-50+'%',
-		opacity:'0',
-		filter:'alpha(opacity=0)',
-	},800);
-	ele.lis.eq(inf.curp+1).animate({
-		left:ele.lis.eq(inf.curp+1).css('left').slice('%')[0]+50+'%',
-		opacity:'1',
-		filter:'alpha(opacity=100)',
-	},800);
-})
+ele.leftArr.on('click', funs.leftEvent);
+ele.rightArr.on('click', funs.rightEvent);
